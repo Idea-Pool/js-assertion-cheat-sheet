10f96f1a8d4d487be370a83d9258c0fab39908d6
Marta Nagy <kyoukaa98@gmail.com>
2024-03-29 21:17:43 +0100

    squash previous work
    add placeholder index.html
    
    add workflows for feature branch deployment/cleanup
    
    update trigger for testing purposes
    
    update branch name verification
    
    add AWS region
    
    downgrade to configure-aws-credentials@v2
    
    Revert "downgrade to configure-aws-credentials@v2"
    
    This reverts commit 51054919e3d51a5086df3fa8da675c10a074633f.
    
    update role-to-assume
    
    add cdk bootstrap
    
    add cdk bootstrap
    
    trying to clean up
    
    add region
    
    remove bootstrapping
    
    re-add bootstrap
    
    debug
    
    Revert "debug"
    
    This reverts commit c1d9fb76d9a117efeca6177ea2ba4413262f1d9f.
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    restore earlier state
    
    remove bootstrapping

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { DocsSiteTemplate } from '../lib/cdk-stack';
+import { JsAssertionCheatSheet } from '../lib/cdk-stack';
+
+const STACK_NAME_MAX_LENGTH = 128;
+const DEFAULT_STACK_NAME = 'JsAssertionCheatSheet';
 
 const app = new cdk.App();
-new DocsSiteTemplate(app, 'DocsSiteTemplate', {
+const branchId = app.node.tryGetContext('branch_id');
+const branchSuffix = branchId ? `-${branchId}` : '';
+const stackName = `${DEFAULT_STACK_NAME}${branchSuffix}`.slice(0, STACK_NAME_MAX_LENGTH);
+
+new JsAssertionCheatSheet(app, stackName, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
