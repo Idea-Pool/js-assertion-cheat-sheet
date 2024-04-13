4c5c83774ff1241d7894c47bb77c7002da288ce4
Marta Nagy <kyoukaa98@gmail.com>
2024-04-13 17:48:43 +0200

    update workflows based on the new approach

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,14 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { JsAssertionCheatSheet } from '../lib/cdk-stack';
 
-const STACK_NAME_MAX_LENGTH = 128;
-const DEFAULT_STACK_NAME = 'JsAssertionCheatSheet';
-
 const app = new cdk.App();
-const branchId = app.node.tryGetContext('branch_id');
-const branchSuffix = branchId ? `-${branchId}` : '';
-const stackName = `${DEFAULT_STACK_NAME}${branchSuffix}`.slice(0, STACK_NAME_MAX_LENGTH);
-
-new JsAssertionCheatSheet(app, stackName, {
+new JsAssertionCheatSheet(app, 'JsAssertionCheatSheet', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
