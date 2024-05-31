bb8d75a1b1f74c07436ccf32d687ba85b39e3bfc
Tomasz Fijarczyk <tomasz.fijarczyk_rft@thomsonreuters.com>
2024-05-31 09:57:12 +0200

    Post-rebase fixes

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,10 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { JsAssertionCheatSheet } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-<<<<<<< HEAD
 new JsAssertionCheatSheet(app, 'JsAssertionCheatSheet', {
-=======
-new DocsSiteTemplate(app, 'JsAssertionCheatSheet', {
->>>>>>> c0b32b8 (Add main deployment first draft)
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
